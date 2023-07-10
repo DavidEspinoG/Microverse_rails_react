93818c734fc859f46c0ee06c8aea0ff3ba117b0e
David Espino <davidespinog@gmail.com>
2023-07-09 18:47:34 -0600

    Add redux and asyncthunk

--- app/javascript/Greeting.js
+++ app/javascript/Greeting.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { fetchGreetings } from "./greetingSlice";
 
 function Greeting() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(fetchGreetings());
+    console.log('use effect') 
+  })
   return (
     <div>
       <Link to="/">Home</Link>
