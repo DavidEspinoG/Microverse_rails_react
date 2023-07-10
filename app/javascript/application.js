1dfe7d569438edbbb5ab6336efe4275f51df2fe8
David Espino <davidespinog@gmail.com>
2023-07-09 18:03:46 -0600

    Add Greeting component

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,10 +3,12 @@
 // import "./controllers"
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 ReactDOM.render(
-  <App/>,
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>,
   document.getElementById('root'),
 );
